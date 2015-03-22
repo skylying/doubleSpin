0af9b52921bb00797a37ee77728eab6cb9539b42
skylying <linsutim@gmail.com>
2015-03-22 16:55:43 +0800

    connect default config and user config

--- dd.js
+++ dd.js
@@ -1,21 +1,51 @@
-//TODO: Merge default config - done
-//TODO: 避免全局污染, 改用 self-execute function 寫 - done
-//TODO: Cross browser css support
+// done TODO: Merge default config
+// done TODO: 避免全局污染, 改用 self-execute function 寫
+// done TODO: Cross browser css support
+// done TODO: add node to constructor
+// done TODO: Insert config to style list
 //TODO: Insert @keyframe
-//TODO: add node to constructor
 //TODO: Add user agent to define browser
+//TODO: Insert IE css support
 //TODO: setInterval() alternative
 
 (function()
 {
-
-	var prefix = ['webkit', 'moz', 'o', 'ms'];
+	var prefixList = ['webkit', 'Moz', 'O', 'ms'];
 
 	var defaultConfig = {
 		dimension: 100,
 		topColor: '#FF0000',
 		bottomColor: '#008000',
 		speed: 'fast'
+	};
+
+	var styleList = {
+		spinner: {
+			"position": 'absolute',
+			"left": '50%',
+			"top": '50%'
+		},
+		spinnerInner : {
+			"width": '100px',
+			"height": '100px',
+			"position": 'absolute',
+			"BorderRadius": '50%',
+			"marginLeft": '-50px',
+			"marginTop": '-50px'
+		},
+		top: {
+			"background": 'red',
+			"animation": 'leftspin linear infinite',
+			"animationDuration": '2s',
+			"zIndex": '100'
+		},
+		bottom: {
+			"background": 'green',
+			"animation": 'rightspin linear infinite',
+			"animationDuration": '2s',
+			"transform": 'scale(0.4)',
+			"zIndex": '1'
+		}
 	};
 
 	/**
@@ -36,6 +66,12 @@
 		return e;
 	}
 
+	/**
+	 * Append html element to parent
+	 *
+	 * @param parent
+	 * @returns {object}
+	 */
 	function insert(parent /* Node1, node2, ...*/)
 	{
 		var l = arguments.length;
@@ -51,15 +87,52 @@
 		return el.sheet || el.styleSheet;
 	}());
 
-	function extendObj(extraObj, defaultObj)
+	function extendObj(base /* ext1, ext2, ....*/)
 	{
-		var p;
-		for (p in defaultObj) {
-			if (extraObj[p] === undefined) {
-				extraObj[p] = defaultObj[p];
+		var l = arguments.length,
+			p;
+		for (var i=1; i<l; i++) {
+			for (p in arguments[i]) {
+				if (base[p] === undefined) {
+					base[p] = arguments[i][p];
+				}
 			}
 		}
-		return extraObj;
+
+		return base;
+	}
+
+	function addStyle(node /* style1, style2, ....*/)
+	{
+		var l = arguments.length,
+			p;
+		for (var i=1; i<l; i++) {
+			for (p in arguments[i]) {
+				node.style[vendor(node, p)] = arguments[i][p];
+			}
+		}
+	}
+
+	/**
+	 * Detect browser supported css3 style prefix
+	 *
+	 * @param node
+	 * @param prop
+	 */
+	function vendor(node, prop)
+	{
+		var l = prefixList.length,
+			p = prop.charAt(0).toUpperCase() + prop.slice(1);
+
+		for (var i=0; i<l; i++) {
+			var pp = prefixList[i] + p;
+			if (node.style[pp] !== undefined) {
+				return pp;
+			}
+			if (node.style[prop] !== undefined) {
+				return prop;
+			}
+		}
 	}
 
 	/**
@@ -72,85 +145,57 @@
 	function DoubleSpin(node, config)
 	{
 		this.node = node;
+		this.config = {};
 
-		// speed setter
+		//speed setter
 		this.config = {
 			set speed(speed) {
+
+				if (this.animationSpeed !== undefined) return;
+
 				switch (speed) {
 					case 'fast':
 						return this.animationSpeed = '2s';
+						break;
 					case 'medium':
 						return this.animationSpeed = '4s';
+						break;
 					case 'slow':
 						return this.animationSpeed = '6s';
+						break;
+					default :
+						return this.animationSpeed = '2s';
+						break;
 				}
 			}
 		};
 
+		addStyle(node, styleList.spinner);
+
 		// Merge config
-		extendObj(this.config, defaultConfig);
+		extendObj(this.config, config, defaultConfig);
+
+		// Set style
+		var c = this.config;
+
+		styleList.spinnerInner.width = styleList.spinnerInner.height = c.dimension + 'px';
+		styleList.spinnerInner.marginLeft = styleList.spinnerInner.marginTop = '-' + (parseInt(c.dimension) / 2) + 'px';
+		styleList.top.animationDuration = styleList.bottom.animationDuration = c.animationSpeed;
+		styleList.top.background = c.topColor;
+		styleList.bottom.background = c.bottomColor;
 	}
 
 	DoubleSpin.prototype = {
 		spin: function()
 		{
-			var leftSpinner = createElement('div', {className: 'spinner-inner top'}),
-				rightSpinner = createElement('div', {className: 'spinner-inner bottom'});
+			var topSpinner = createElement('div', {className: 'spinner-inner top'}),
+				bottomSpinner = createElement('div', {className: 'spinner-inner bottom'}),
+				s = styleList;
 
-			insert(this.node, leftSpinner, rightSpinner);
-			this.addStyle()
-		},
-		addStyle: function()
-		{
-			var classList = ['#spinner', '.spinner-inner', '.top', '.bottom'],
-				l = classList.length,
-				c = this.config;
+			addStyle(topSpinner, s.spinnerInner, s.top);
+			addStyle(bottomSpinner, s.spinnerInner, s.bottom);
 
-			for (var i = 0; i < l; i++)
-			{
-				var className = classList[i];
-
-				sheet.insertRule(
-					className + this.getClass(className, c), sheet.cssRules.length
-				)
-			}
-		},
-		getClass: function(className, c)
-		{
-			switch(className){
-				case '#spinner':
-					return '{' +
-						'width: 200px;' +
-						'height: 200px;' +
-						'background: #ddd;' +
-						'position: relative;' +
-						'left: 40%;' +
-						'}';
-
-				case '.spinner-inner':
-					return '{' +
-						'width:' + c.dimension + 'px;' +
-						'height:' + c.dimension + 'px;' +
-						'position : absolute;' +
-						'border-radius: 50%;' +
-						'}';
-
-				case '.top':
-					return '{' +
-						'background:' + c.topColor +';' +
-						'margin-left:' + (c.dimension / 2) + 'px;' +
-						'-webkit-animation: leftspin ' + c.animationSpeed + ' linear infinite;' +
-						'z-index: 100;' +
-						'}';
-
-				case '.bottom':
-					return '{' +
-						'background:' + c.bottomColor + ';' +
-						'margin-left:' + (c.dimension / 2) + 'px;' +
-						'-webkit-transform: scale(0.4);' +
-						'-webkit-animation: rightspin ' + c.animationSpeed + ' linear infinite;z-index: 1;' +
-						'}';
-			}
+			insert(this.node, topSpinner, bottomSpinner);
 		}
 	};
 
