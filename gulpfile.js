1c8e3d718be01ce1119d0fba926f46bb68233978
skylying <linsutim@gmail.com>
2015-04-09 21:07:50 +0800

    update gulp file, add live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
- 
-gulp.task('compress', function() {
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	connect = require('gulp-connect');
+
+// Start webserver
+gulp.task('webserver', function() {
+	connect.server({
+		livereload: true
+	});
+});	
+
+// Minify js
+gulp.task('minify', function() {
   gulp.src('src/dd.js')
     .pipe(uglify())
     .pipe(rename({
     	extname: '.min.js'
     }))
     .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
 });
+
+// reload
+gulp.task('reload', function() {
+	gulp.src('index.html')
+		.pipe(connect.reload());	
+})
+
+gulp.task('watch', function() {
+	gulp.watch('src/**/*.*', ['minify']);
+	gulp.watch('index.html', ['reload']);
+});
+
+gulp.task('default', ['webserver', 'watch']);
